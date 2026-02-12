647421604cb90bbb48e14c1e0f2c27cad30a07c4
changhui98 <net_0102@naver.com>
2026-02-12 22:32:09 +0900

    Feat : 유저 엔티티 초기 세팅

---
+++ src/main/java/com/sph/location_user/user/domain/entity/User.java
@@ -0,0 +1,50 @@
+package com.sph.location_user.user.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+import org.locationtech.jts.geom.Point;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "p_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String address;
+
+    @JdbcTypeCode(SqlTypes.GEOGRAPHY)
+    @Column(nullable = false, columnDefinition = "geography(Point,4326)")
+    private Point location;
+
+    public static User of(
+        String username,
+        String password,
+        String address,
+        Point location
+    ) {
+        User user = new User();
+        user.username = username;
+        user.password = password;
+        user.address = address;
+        user.location = location;
+        return user;
+    }
+}
