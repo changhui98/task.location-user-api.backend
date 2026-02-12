fc5550c436a6d8d594186e666edde9462565f03a
changhui98 <net_0102@naver.com>
2026-02-12 21:55:19 +0900

    Feat : init commit

---
+++ src/main/java/com/sph/location_user/LocationUserApplication.java
@@ -0,0 +1,13 @@
+package com.sph.location_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sph/location_user/LocationUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sph.location_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
