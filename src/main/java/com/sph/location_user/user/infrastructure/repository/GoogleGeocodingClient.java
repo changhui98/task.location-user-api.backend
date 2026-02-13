c7dfddc6df5e11f4d661dae36f00acac97f4417a
changhui98 <net_0102@naver.com>
2026-02-13 20:56:19 +0900

    Feat : 사용자가 입력한 주소 위/경도 좌표 변환

--- src/main/java/com/sph/location_user/user/infrastructure/repository/GoogleGeocodingClient.java
+++ src/main/java/com/sph/location_user/user/infrastructure/repository/GoogleGeocodingClient.java
@@ -1,13 +1,46 @@
 package com.sph.location_user.user.infrastructure.repository;
 
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class GoogleGeocodingClient implements GeocodingClient {
 
+    @Value("${google.maps.api-key}")
+    private String apiKey;
+
+    private final WebClient webClient = WebClient.create(
+        "https://maps.googleapis.com"
+    );
+
     @Override
     public GeoPoint convert(String address) {
 
-        return new GeoPoint(37.0, 127.0);
+        Map response = webClient.get()
+            .uri(uriBuilder -> uriBuilder
+                .path("/maps/api/geocode/json")
+                .queryParam("address", address)
+                .queryParam("key", apiKey)
+                .build())
+            .retrieve()
+            .bodyToMono(Map.class)
+            .block();
+
+        if (response == null || !"OK".equals(response.get("status"))) {
+            throw new RuntimeException("주소 변환 실패");
+        }
+
+        List results =  (List) response.get("results");
+        Map first = (Map) results.get(0);
+        Map geometry = (Map) first.get("geometry");
+        Map location = (Map) geometry.get("location");
+
+        double lat = ((Number) location.get("lat")).doubleValue();
+        double lng = ((Number) location.get("lng")).doubleValue();
+
+        return new GeoPoint(lat, lng);
     }
 }
